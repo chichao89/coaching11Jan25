c01c8a3a357f72514584c5994e4f250f8a5a6b27
Javier See <70385670+chichao89@users.noreply.github.com>
2025-01-11 12:16:12 +0800

    my third commit

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a simple git repository
 
 line 2
 
+git log -- show history of commits
+git reset <file>
+git ignore file 
--- app.py
+++ app.py
@@ -2,4 +2,3 @@ import utils
 
 utils.greeting("chichao")
 
-#git remote add origin https://github.com/chichao89/coaching11Jan25.git
